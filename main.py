7f23bca9214558d70795853997395f3560a68e9e
aishk5500 <aishwaryak017@gmail.com>
2023-12-24 01:11:54 +0530

    copy button added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import tkinter
 import pyshorteners
+import pyperclip
 def shorten():
     shortner = pyshorteners.Shortener()
     short_url = shortner.tinyurl.short(longurl_entry.get())
     print(shorturl_entry.insert(0,short_url))
+def copyurl():
+   copy_url=shorturl_entry.get()
+   pyperclip.copy(copy_url)
 
 
 root = tkinter.Tk()
@@ -14,9 +18,11 @@ longurl_entry = tkinter.Entry(root)
 shorturl_label = tkinter.Label(root, text="shortened URL")
 shorturl_entry =  tkinter.Entry(root)
 shorturl_button = tkinter.Button(root, text="Shorten URL", command=shorten)
+copyurl_button = tkinter.Button(root, text="Copy URL", command=copyurl)
 longurl_label.pack()
 longurl_entry.pack()
 shorturl_label.pack()
 shorturl_entry.pack()
 shorturl_button.pack()
+copyurl_button.pack()
 root.mainloop()
